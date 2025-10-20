02e957491d9e340cfb4d99f2947dec43d6d7d4b8
KyoheiAbc <autumn8november@gmail.com>
2025-10-20 23:30:19 +0900

    Update Godot configuration and service worker; modify manifest orientation for improved compatibility and caching

--- test/test.gd
+++ test/test.gd
@@ -1,27 +1,2 @@
 class_name Test
 extends Node
-
-var rival = Rival.new()
-var ui = UI.new()
-static var FRAME_COUNT: int = 0
-
-func _ready():
-	add_child(rival)
-	add_child(ui)
-
-	FRAME_COUNT = 0
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if not event.pressed:
-			return
-		if event.keycode == KEY_W or event.keycode == KEY_UP:
-			ui.combo(true)
-		if event.keycode == KEY_S or event.keycode == KEY_DOWN:
-			ui.action_motion(true)
-		if event.keycode == KEY_A or event.keycode == KEY_LEFT:
-			ui.combo(false)
-		if event.keycode == KEY_D or event.keycode == KEY_RIGHT:
-			ui.action_motion(false)
-		if event.keycode == KEY_SPACE or event.keycode == KEY_ENTER:
-			ui.game_over(true)
