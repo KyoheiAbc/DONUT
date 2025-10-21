0b387ab99a9ea504683ee8e4650517ce618367fc
KyoheiAbc <autumn8november@gmail.com>
2025-10-21 23:37:13 +0900

    Update Godot configuration: increment CACHE_VERSION for service worker and adjust index.pck file size

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760970597|20088628';
+const CACHE_VERSION = '1761057417|23481566';
 /** @type {string} */
 const CACHE_PREFIX = 'Godot Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
