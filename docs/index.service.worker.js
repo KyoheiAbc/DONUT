155b809009dd41b402afcdbebe824bcd9fc16e3d
KyoheiAbc <autumn8november@gmail.com>
2025-10-29 22:28:52 +0900

    Update cache version in service worker and adjust file size in Godot config for improved resource management.

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761659572|24697555';
+const CACHE_VERSION = '1761744513|27103855';
 /** @type {string} */
 const CACHE_PREFIX = 'Godot Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
