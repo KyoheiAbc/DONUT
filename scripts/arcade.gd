dc36db762592c7cd67f85dcf40f09f896eaa38e9
KyoheiAbc <autumn8november@gmail.com>
2025-10-30 21:52:39 +0900

    Adjust sprite positions for better alignment and update label position for consistency

--- scripts/arcade.gd
+++ scripts/arcade.gd
@@ -7,20 +7,20 @@ func _ready() -> void:
 	var sprite_a = Sprite2D.new()
 	add_child(sprite_a)
 	sprite_a.texture = Character.SPRITES[Main.PLAYER_CHARACTER_INDEX]
-	sprite_a.position = Vector2(600, 275)
+	sprite_a.position = Vector2(600, 300)
 	
 	var sprite_b = Sprite2D.new()
 	add_child(sprite_b)
 	Main.RIVAL_CHARACTER_INDEX = Main.ARCADE_RIVAL_CHARACTER_INDEXES[Main.ARCADE_LEVEL]
 	sprite_b.texture = Character.SPRITES[Main.RIVAL_CHARACTER_INDEX]
-	sprite_b.position = Vector2(1400, 275)
+	sprite_b.position = Vector2(1400, 300)
 
 	for i in range(Main.ARCADE_RIVAL_CHARACTER_INDEXES.size()):
 		var sprite = Sprite2D.new()
 		add_child(sprite)
 		sprite.texture = Character.SPRITES[Main.ARCADE_RIVAL_CHARACTER_INDEXES[i]]
 		sprite.scale = Vector2(0.45, 0.45)
-		sprite.position = Vector2(i * 220, 725)
+		sprite.position = Vector2(i * 220, 685)
 		sprite.position.x += (Main.WINDOW.x - (7) * 220) / 2
 
 		var color_rect = ColorRect.new()
@@ -38,8 +38,12 @@ func _ready() -> void:
 	var label = Main.label_new()
 	add_child(label)
 	label.text = "VS"
-	label.position.y = 275 - label.size.y / 2
+	label.position.y = 300 - label.size.y / 2
 
-	await get_tree().create_timer(1.5).timeout
-	queue_free()
-	Main.NODE.add_child(Game.new())
+	var button_start = Main.button_new()
+	add_child(button_start)
+	button_start.text = "START"
+	button_start.pressed.connect(func() -> void:
+		self.queue_free()
+		Main.NODE.add_child(Game.new())
+	)
